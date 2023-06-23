b8ef8ee4a9bf4b1493bcc6686823f1ad3e7ddd79
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2023-06-23 14:32:51 +0300

    Клонування ДЗ

---
+++ src/redux/operations.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://648ec23e75a96b6644443d57.mockapi.io';
+
+export const fetchContacts = createAsyncThunk(
+  "contacts/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/contacts");
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+)
+export const addContact = createAsyncThunk(
+  "contacts/addContact",
+  async ({ name, phone }, thunkAPI) => {
+    try {
+      const response = await axios.post("/contacts", { name: name, phone: phone, });
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  "contacts/deleteContact",
+  async (contactId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/contacts/${contactId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
